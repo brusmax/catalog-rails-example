9c786d6ae0b6dc9a675cdd68ed897b7160c15605
Brusmax <enrique.webgenius@gmail.com>
2015-02-06 23:17:45 +1300

    pagination added

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,5 @@
 class LandingController < ApplicationController
   def index
-  	@products = Product.includes(:category).all
+  	@products = Product.includes(:category).paginate(:page => params[:page], :per_page => 12)
   end
 end
